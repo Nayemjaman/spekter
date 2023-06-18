469ff785627bc85e0779e5a5801a06569dcc1336
Nayem Jaman Tusher <njaman26@gmail.com>
2023-06-18 17:35:31 +0600

    views

--- backend/sentiment/views.py
+++ backend/sentiment/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+
+from rest_framework.generics import CreateAPIView
+from rest_framework.response import Response
+from transformers import pipeline
 # Create your views here.
+class SentimentAnalysisView(CreateAPIView):
+    def create(self, request, *args, **kwargs):
+        text = request.data.get('text')
+        if not text:
+            return Response({'error': 'Text is required.'}, status=400)
+
+        # Perform sentiment analysis using a pre-trained model
+        sentiment_classifier = pipeline('sentiment-analysis')
+        result = sentiment_classifier(text)[0]
+        sentiment = result['label']
+
+        return Response({'sentiment': sentiment})
